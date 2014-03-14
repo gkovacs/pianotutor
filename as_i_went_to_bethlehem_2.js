2ea40bc9ba76765ad19b67d99cc7506c672d7521
Geza Kovacs <gkovacs@stanford.edu>
2014-03-14 14:41:04 -0700

    added single-part task

---
+++ as_i_went_to_bethlehem_2.js
@@ -0,0 +1,8 @@
+setCurriculum([
+  {
+    "exercises": [
+      "C2_0.5 D2_0.5 E2_1.0 D2_1.0 C2_1.0 A1_1.0 E1_1.0 C2_0.5 D2_0.5 E2_1.0 D2_1.0 C2_3.0 B1_0.75 C2_0.25 D2_0.5 D2_0.5 E2_0.5 D2_0.5 C2_1.0 A1_1.0 E1_1.0 B1_0.5 C2_0.5 D2_1.0 C2_0.5 B1_0.5 A1_3.0"
+    ], 
+    "name": "As_I_Went_To_Bethlehem_2.xml_part0_full"
+  }
+])
